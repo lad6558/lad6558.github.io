45aa76d64d51f814a8aa5810f6f6d6ffa97b658f
Andi Liu <lad6558@gmail.com>
2024-03-02 23:08:21 -0500

    change favicon, add top bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
-    import "../app.css";
+	import '../app.css';
 </script>
-  
+
+<nav class="flex flex-row space-x-8 pt-4 font-semibold justify-center text-lg">
+	<a href="/"> Home </a>
+	<a href="/blogs"> Blogs </a>
+	<a href="/projects"> Projects </a>
+</nav>
+<div class="divider"></div>
 <slot />
