1ed45cae881e568d3af1b62100ad4c72c91f8829
Andi Liu <lad6558@gmail.com>
2024-03-03 00:13:27 -0500

    Updates

---
+++ app/immutable/nodes/2.zZgqzBsr.js
@@ -0,0 +1,9 @@
+import{s as h,e as m,a as f,h as p,c as o,d as n,b as x,g as v,f as l,i as b,j as u,n as r}from"../chunks/scheduler.jkEgMtR5.js";import{S as g,i as _}from"../chunks/index.-ncuGFxm.js";const L=""+new URL("../assets/Profile Pic.1G-LtXvV.png",import.meta.url).href,w=""+new URL("../assets/Andi_Resume_Aug_11.hKDh0ZxQ.pdf",import.meta.url).href;function A(c){let e,s,t,d=`<div><div class="text-5xl mt-4 ml-2">Andi Liu 刘安迪</div> <p class="mt-4">Hi! I am Andi Liu. I study at MIT, majoring in Computer Science.
+			<br/>
+			In my free time I enjoy blogging and building software.</p> <p class="text-center mt-2"><a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="mailto:andiliu@mit.edu">Email</a>
+			/
+			<a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="${w}">Resume</a>
+			/
+			<a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://github.com/lad6558">GitHub</a>
+			/
+			<a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://www.linkedin.com/in/andi-liu/">LinkedIn</a></p></div> <div class="ml-8"><img class="max-w-full h-auto" src="${L}" alt="Andi"/></div>`;return{c(){e=m("meta"),s=f(),t=m("div"),t.innerHTML=d,this.h()},l(a){const i=p("svelte-1smrbyy",document.head);e=o(i,"META",{name:!0,content:!0}),i.forEach(n),s=x(a),t=o(a,"DIV",{class:!0,"data-svelte-h":!0}),v(t)!=="svelte-1iglr2t"&&(t.innerHTML=d),this.h()},h(){document.title="Andi Liu",l(e,"name","description"),l(e,"content","Hi! This is Andi Liu, a CS undergrad from MIT"),l(t,"class","relative flex flex-col sm:flex-row")},m(a,i){b(document.head,e),u(a,s,i),u(a,t,i)},p:r,i:r,o:r,d(a){a&&(n(s),n(t)),n(e)}}}class k extends g{constructor(e){super(),_(this,e,null,A,h,{})}}export{k as component};
