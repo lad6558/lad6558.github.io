47cdef4d4abdfcdb8cc6a37914efe08d3ba1feb1
Andi Liu <lad6558@gmail.com>
2024-03-17 17:36:50 -0400

    Updates

---
+++ app/immutable/entry/start.hIpyhRkG.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.-pGtE7rS.js";export{t as start};
---
+++ app/immutable/nodes/2.49GFsAzx.js
@@ -0,0 +1,12 @@
+import{s as f,e as o,a as h,h as p,c as m,d as s,b as x,g as b,f as r,i as v,j as c,n as l}from"../chunks/scheduler.rrxIz5D8.js";import{S as k,i as g}from"../chunks/index.FkS-bMAd.js";const w=""+new URL("../assets/Profile Pic.1G-LtXvV.png",import.meta.url).href,_=""+new URL("../assets/Profile Pic Dark.4SSQbYZZ.jpeg",import.meta.url).href,I=""+new URL("../assets/Andi_Resume_Jan_25.uFhMcLoi.pdf",import.meta.url).href;function L(u){let e,n,t,d=`<div class="flex items-center justify-center"><img class="h-80 w-64 dark:hidden" src="${w}" alt="Andi"/> <img class="h-80 w-64 hidden dark:block" src="${_}" alt="Andi"/></div> <div class="mt-4 flex flex-col items-center justify-center md:w-[34rem] px-8"><p class="block dark:hidden">Hi! I am Andi Liu. I study at MIT, majoring in Computer Science.
+			<br/>
+			In my free time I enjoy blogging and building software.</p> <p class="hidden dark:block">Hi! I am Andi Liu, builder of software, master of the undead. (i.e. I own a pet skull named
+			Yuri after Yorick from Hamlet)
+			<br/>
+			In my free time I enjoy writing and doing theater.</p> <p class="text-center mt-2"><a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="mailto:andiliu@mit.edu">Email</a>
+			/
+			<a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="${I}">Resume</a>
+			/
+			<a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://github.com/lad6558">GitHub</a>
+			/
+			<a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://www.linkedin.com/in/andi-liu/">LinkedIn</a></p></div>`;return{c(){e=o("meta"),n=h(),t=o("div"),t.innerHTML=d,this.h()},l(a){const i=p("svelte-1smrbyy",document.head);e=m(i,"META",{name:!0,content:!0}),i.forEach(s),n=x(a),t=m(a,"DIV",{class:!0,"data-svelte-h":!0}),b(t)!=="svelte-7jivtd"&&(t.innerHTML=d),this.h()},h(){document.title="Andi Liu",r(e,"name","description"),r(e,"content","Hi! This is Andi Liu, a CS undergrad from MIT"),r(t,"class","relative flex flex-col md:flex-row mx-auto space-x-0 md:space-x-8")},m(a,i){v(document.head,e),c(a,n,i),c(a,t,i)},p:l,i:l,o:l,d(a){a&&(s(n),s(t)),s(e)}}}class A extends k{constructor(e){super(),g(this,e,null,L,f,{})}}export{A as component};
