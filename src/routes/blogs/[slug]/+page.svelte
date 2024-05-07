627f6fc594d0cb2995c714e9ae0e1ef93829693c
Andi Liu <lad6558@gmail.com>
2024-05-06 20:47:19 -0400

    add formating for prism code

--- src/routes/blogs/[slug]/+page.svelte
+++ src/routes/blogs/[slug]/+page.svelte
@@ -3,13 +3,14 @@
 	const { meta, Content } = data;
 
 	import '$lib/styles/blog.css';
+	import '$lib/styles/prism.css';
 </script>
 
 <svelte:head>
 	<title>{data.meta.title}</title>
 </svelte:head>
 
-<main class="max-w-[40em] mx-8 mb-12 mt-4">
+<main class="max-w-[42em] mx-8 mb-12 mt-4">
 	<h1 class="text-4xl font-bold mb-1">{meta.title}</h1>
 	<p class="text-slate-500 mb-1">{meta.description}</p>
 	<p class="text-slate-500 mb-4 text-sm">{meta.date}</p>
