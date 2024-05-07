79d6ddc1484a89998f73d565d660886038f362e2
Andi Liu <lad6558@gmail.com>
2024-05-06 22:26:30 -0400

    add mobile support (except code blocks)

--- src/routes/blogs/[slug]/+page.svelte
+++ src/routes/blogs/[slug]/+page.svelte
@@ -10,7 +10,7 @@
 	<title>{data.meta.title}</title>
 </svelte:head>
 
-<section class="max-w-prose mx-8 mb-12 mt-4">
+<section class="max-w-prose w-full mb-12 mt-4 px-4">
 	<h1 class="text-4xl font-bold mb-2">{meta.title}</h1>
 	<p class="text-slate-500 dark:text-slate-400 mb-1">{meta.description}</p>
 	<p class="text-slate-500 dark:text-slate-400 mb-8 text-sm">{meta.date}</p>
