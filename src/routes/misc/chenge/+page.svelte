643386ebb8a97a0dbaaf17870bb8a3848011dd67
Andi Liu <lad6558@gmail.com>
2024-11-24 22:28:30 -0500

    add confetti to a very special page

---
+++ src/routes/misc/chenge/+page.svelte
@@ -0,0 +1,85 @@
+<script>
+	import lineDog from '$lib/assets/Line Dog.gif';
+	import { Confetti } from 'svelte-confetti';
+
+	let showClickConfetti = false;
+	let clickConfetti = [];
+	let timeout;
+	const duration = 2000;
+
+	function spawnConfetti(event) {
+		const { clientX, clientY } = event;
+		clickConfetti = [...clickConfetti, { x: clientX, y: clientY }];
+
+		clearTimeout(timeout);
+		timeout = setTimeout(() => (clickConfetti = []), duration);
+	}
+</script>
+
+<!-- Fullscreen confetti container -->
+<div
+	style="position: fixed; top: -50px; left: 0; height: 100vh; width: 100vw; display: flex; justify-content: center; overflow: hidden; pointer-events: none;"
+>
+	<!-- Main confetti stream -->
+	<Confetti
+		x={[-5, 5]}
+		y={[0, 0.1]}
+		delay={[0, 2000]}
+		infinite
+		duration={10000}
+		amount={800}
+		size="15"
+		fallDistance="100vh"
+		colorArray={['#FFD700', '#FFA500', '#FF69B4', '#FF1493', '#FF4500']}
+	/>
+
+	<!-- Additional confetti streams for more coverage -->
+	<Confetti
+		x={[-7, -2]}
+		y={[0, 0.1]}
+		delay={[500, 2500]}
+		infinite
+		duration={4500}
+		amount={200}
+		fallDistance="100vh"
+		colorArray={['#FFD700', '#FFA500', '#FF69B4']}
+	/>
+	<Confetti
+		x={[2, 7]}
+		y={[0, 0.1]}
+		delay={[250, 2250]}
+		infinite
+		duration={4800}
+		amount={200}
+		fallDistance="100vh"
+		colorArray={['#FFD700', '#FFA500', '#FF69B4']}
+	/>
+</div>
+
+<div
+	class="flex flex-col max-w-xl px-6 py-12 space-y-4 ring-2 ring-yellow-300"
+	on:click={spawnConfetti}
+>
+	<h2 class="text-2xl font-bold">我亲爱的晨歌，</h2>
+	<p class="text-lg">
+		生日快乐！这个域名是我给你的生日礼物。等到你要做个人网站的时候，我就把这个域名转给你。
+	</p>
+	<p class="text-lg">很开心能遇见你！</p>
+	<img src={lineDog} alt="line dog" class="w-1/2" />
+
+	<p class="text-xl font-bold w-full text-right">刘安迪</p>
+	<p class="w-full text-right">2024年11月24日</p>
+
+	{#each clickConfetti as conf}
+		<div style="position: fixed; left: {conf.x}px; top: {conf.y}px; pointer-events: none;">
+			<Confetti
+				x={[-1.5, 1.5]}
+				y={[-1, 1]}
+				fallDistance="100px"
+				amount={200}
+				{duration}
+				colorArray={['#FFD700', '#FFA500', '#FF69B4', '#FF1493', '#FF4500']}
+			/>
+		</div>
+	{/each}
+</div>
