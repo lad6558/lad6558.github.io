9576c137097874e2d2048b62e47f7e1de5f0571f
Andi Liu <lad6558@gmail.com>
2025-04-13 23:36:38 -0400

    Updates

---
+++ app/immutable/entry/start.o1q3PN_j.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.N9APB97q.js";export{t as start};
---
+++ app/immutable/nodes/3.3nUSgfEP.js
@@ -0,0 +1,50 @@
+import{s as N,e,d as b,t as I,h as W,f as n,m as w,j as s,k as L,l as A,v as C,n as a,L as F,o as t,p as Z,q as V}from"../chunks/scheduler.D28vW62Q.js";import{S as K,i as X}from"../chunks/index.r4fslLOX.js";const tt=""+new URL("../assets/profile4to5.OcVV4lNI.jpeg",import.meta.url).href,et=""+new URL("../assets/Profile Pic Dark.dn_nQ1r8.jpeg",import.meta.url).href,nt=""+new URL("../assets/Resume_Aug_10_Andi_Liu.RgfgAQyJ.pdf",import.meta.url).href;function at(J){let d,i,Y="",q,p,U=`window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-G035D2MZSG');
+	`,u,O=`{
+			"@context": "https://schema.org",
+			"@type": "Person",
+			"name": "Andi Liu",
+			"url": "https://andiliu.me",
+			"image": "https://andiliu.me/app/immutable/assets/Profile%20Pic.49AGY8G_.jpeg",
+			"sameAs": [
+				"https://www.linkedin.com/in/andiliu/",
+				"https://github.com/lad6558"
+			],
+			"jobTitle": "Computer Science Student",
+			"alumniOf": "MIT",
+			"homeLocation": {
+				"@type": "Place",
+				"address": {
+					"@type": "PostalAddress",
+					"addressLocality": "Cambridge",
+					"addressRegion": "MA"
+				}
+			},
+			"email": "mailto:andiliu@mit.edu",
+			"description": "Hi! This is Andi Liu, a CS undergrad from MIT. You can check out my blogs and software projects here!",
+			"knowsAbout": ["Software Development", "AI", "Machine Learning", "Blogging", "Theater"],
+			"subjectOf": {
+				"@type": "CreativeWork",
+				"name": "Resume",
+				"url": "https://andiliu.me/app/immutable/assets/Resume_Aug_10_Andi_Liu.pdf"
+			}
+		}`,y,o,h,$=`<img class="h-[25rem] w-[20rem] dark:hidden rounded-lg" src="${tt}" alt="Andi"/> <img class="h-[25rem] w-[20rem] hidden dark:block rounded-lg" src="${et}" alt="Andi"/>`,T,r,f,z=`Hi! I am Andi Liu. I study Computer Science at MIT.
+			<br/>
+			In my free time I enjoy blogging and building software.`,M,l,P,c,B="1",j,H,S,R,D,G,g,Q=`<a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="mailto:andiliu@mit.edu">Email</a>
+			/
+			<a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="${nt}">Resume</a>
+			/
+			<a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://github.com/lad6558">GitHub</a>
+			/
+			<a class="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://www.linkedin.com/in/andiliu/">LinkedIn</a>`;return{c(){d=e("meta"),i=e("script"),i.innerHTML=Y,p=e("script"),p.textContent=U,u=e("script"),u.textContent=O,y=b(),o=e("div"),h=e("div"),h.innerHTML=$,T=b(),r=e("div"),f=e("p"),f.innerHTML=z,M=b(),l=e("p"),P=I("Hi! I am Andi Liu, builder of software, master of the undead "),c=e("sup"),j=I(B),H=b(),S=I(`.
+
+			`),R=e("br"),D=I(`
+			In my free time I enjoy writing and doing theater.`),G=b(),g=e("p"),g.innerHTML=Q,this.h()},l(v){const m=W("svelte-1q7h62t",document.head);d=n(m,"META",{name:!0,content:!0}),i=n(m,"SCRIPT",{src:!0,"data-svelte-h":!0}),w(i)!=="svelte-yruedn"&&(i.innerHTML=Y),p=n(m,"SCRIPT",{"data-svelte-h":!0}),w(p)!=="svelte-19x515d"&&(p.textContent=U),u=n(m,"SCRIPT",{type:!0,"data-svelte-h":!0}),w(u)!=="svelte-73vkzv"&&(u.textContent=O),m.forEach(s),y=L(v),o=n(v,"DIV",{class:!0});var k=A(o);h=n(k,"DIV",{class:!0,"data-svelte-h":!0}),w(h)!=="svelte-b09ece"&&(h.innerHTML=$),T=L(k),r=n(k,"DIV",{class:!0});var x=A(r);f=n(x,"P",{class:!0,"data-svelte-h":!0}),w(f)!=="svelte-1vmwep8"&&(f.innerHTML=z),M=L(x),l=n(x,"P",{class:!0});var _=A(l);P=C(_,"Hi! I am Andi Liu, builder of software, master of the undead "),c=n(_,"SUP",{class:!0,"data-tip":!0});var E=A(c);j=C(E,B),H=L(E),E.forEach(s),S=C(_,`.
+
+			`),R=n(_,"BR",{}),D=C(_,`
+			In my free time I enjoy writing and doing theater.`),_.forEach(s),G=L(x),g=n(x,"P",{class:!0,"data-svelte-h":!0}),w(g)!=="svelte-nndxuz"&&(g.innerHTML=Q),x.forEach(s),k.forEach(s),this.h()},h(){document.title="Andi Liu",a(d,"name","description"),a(d,"content","Hi! This is Andi Liu, a CS undergrad from MIT. You can check out my blogs and software projects here!"),i.async=!0,F(i.src,q="https://www.googletagmanager.com/gtag/js?id=G-G035D2MZSG")||a(i,"src",q),a(u,"type","application/ld+json"),a(h,"class","flex items-center justify-center"),a(f,"class","block dark:hidden"),a(c,"class","inline tooltip"),a(c,"data-tip","i.e. I own a pet skull named Yuri after Yorick from Hamlet"),a(l,"class","hidden dark:block"),a(g,"class","text-center mt-2"),a(r,"class","mt-4 flex flex-col items-center justify-center lg:w-[34rem] px-4"),a(o,"class","relative flex flex-col lg:flex-row mx-auto space-x-0 lg:space-x-8")},m(v,m){t(document.head,d),t(document.head,i),t(document.head,p),t(document.head,u),Z(v,y,m),Z(v,o,m),t(o,h),t(o,T),t(o,r),t(r,f),t(r,M),t(r,l),t(l,P),t(l,c),t(c,j),t(c,H),t(l,S),t(l,R),t(l,D),t(r,G),t(r,g)},p:V,i:V,o:V,d(v){v&&(s(y),s(o)),s(d),s(i),s(p),s(u)}}}class rt extends K{constructor(d){super(),X(this,d,null,at,N,{})}}export{rt as component};
